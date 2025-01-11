b873fdbe0de10f8dc5e2e77437a1eb3538fa1124
tupe12334 <ofek.gabay.he@gmail.com>
2025-01-11 21:46:07 +0200

    Improve error handling in get_open_source_projects function to handle API request failures

--- os_advisor.py
+++ os_advisor.py
@@ -10,21 +10,21 @@ def get_open_source_projects(
     response = requests.get(
         "https://api.github.com/search/repositories?q=stars:>10000&sort=stars"
     )
-    if response.status_code == 200:
-        data = response.json()
-        projects = [
-            {
-                "name": item["name"],
-                "url": item["html_url"],
-                "open_issues": item["open_issues_count"],
-                "pull_requests": get_pull_requests_count(item["pulls_url"]),
-                "request_time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            }
-            for item in data["items"]
-        ]
-        return projects
-    else:
+    if response.status_code != 200:
+        print("Failed to fetch data from GitHub API")
         return []
+    data = response.json()
+    projects = [
+        {
+            "name": item["name"],
+            "url": item["html_url"],
+            "open_issues": item["open_issues_count"],
+            "pull_requests": get_pull_requests_count(item["pulls_url"]),
+            "request_time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        }
+        for item in data["items"]
+    ]
+    return projects
 
 
 def get_pull_requests_count(pulls_url: str) -> int:
